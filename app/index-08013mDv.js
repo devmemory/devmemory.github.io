96268b50cbe629c91c3f6a9176d4b0d4d495969c
devmemory <devmemory@users.noreply.github.com>
2025-07-22 15:07:53 +0000

    deploy: devmemory/react_portfolio@8fa2c55d6c352fcfaaa6e2cc48dea7144fb2804c

---
+++ app/index-08013mDv.js
@@ -0,0 +1,1 @@
+import{j as t}from"./three-B6j1n8rj.js";import"./react-DOgAZ9X_.js";const s="_div_container_hhwu5_1",a={div_container:s},n=()=>t.jsx("div",{className:a.div_container,children:t.jsx("iframe",{src:"/assets/data/cv.html"})});export{n as default};
